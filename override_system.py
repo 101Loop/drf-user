e580bed57833c0121bc730f6239c2216e743b614
Himanshu Shankar <amhssingh@gmail.com>
2017-08-21 07:21:55 +0530

    Adding JWT_PAYLOAD_HANDLER that includes mobile and email in JWT!

--- override_system.py
+++ override_system.py
@@ -127,3 +127,50 @@ class JSONWebTokenAuthenticationQS(BaseJSONWebTokenAuthentication):
             raise exceptions.AuthenticationFailed(msg)
 
         return auth[1]
+
+def jwt_payload_handler(user):
+    import uuid
+
+    from calendar import timegm
+    from datetime import datetime
+
+    from rest_framework_jwt.compat import get_username
+    from rest_framework_jwt.compat import get_username_field
+    from rest_framework_jwt.settings import api_settings
+
+    username_field = get_username_field()
+    username = get_username(user)
+
+    payload = {
+        'user_id': user.pk,
+        'username': username,
+        'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA
+    }
+    if hasattr(user, 'email'):
+        payload['email'] = user.email
+
+    if hasattr(user, 'mobile'):
+        payload['mobile'] = user.mobile
+
+    if hasattr(user, 'name'):
+        payload['name'] = user.name
+
+    if isinstance(user.pk, uuid.UUID):
+        payload['user_id'] = str(user.pk)
+
+    payload[username_field] = username
+
+    # Include original issued at time for a brand new token,
+    # to allow token refresh
+    if api_settings.JWT_ALLOW_REFRESH:
+        payload['orig_iat'] = timegm(
+            datetime.utcnow().utctimetuple()
+        )
+
+    if api_settings.JWT_AUDIENCE is not None:
+        payload['aud'] = api_settings.JWT_AUDIENCE
+
+    if api_settings.JWT_ISSUER is not None:
+        payload['iss'] = api_settings.JWT_ISSUER
+
+    return payload
