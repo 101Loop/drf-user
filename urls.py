8b74f55752b8aa781814b05346d343558f002b06
Himanshu Shankar <amhssingh@gmail.com>
2017-10-01 12:05:01 +0530

    Fixed URL

--- urls.py
+++ urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'register/$', views.Register.as_view(), name='Register'),
     url(r'sendotp/$', views.SendOTP.as_view(), name='Send OTP'),
     url(r'verifyotp/$', views.VerifyOTP.as_view(), name='Verify OTP'),
-    url(r'loginotp/$', views.VerifyOTP.as_view(), name='Login OTP'),
+    url(r'loginotp/$', views.LoginOTP.as_view(), name='Login OTP'),
     url(r'isunique/$', views.CheckUnique.as_view(), name='Check Unique'),
 ]
