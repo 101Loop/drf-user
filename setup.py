547c5835bb934c6cfdd42f9ce7f9db8e8918c445
Himanshu Shankar <me@himanshus.com>
2018-07-16 12:11:26 +0530

    Modified for final release

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     url="https://github.com/civilmachines/drf-user",
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     packages=setuptools.find_packages(),
+    install_requires=open('requirements.txt').read().split(),
     include_package_data=True,
     classifiers=(
         'Development Status :: 5 - Production/Stable',
