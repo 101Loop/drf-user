3a3288173703af8e38332b2e72834052dbbfe3dd
Himanshu Shankar <me@himanshus.com>
2018-10-15 15:32:53 +0530

    Major modification for next release
    1. Now mails are being sent via `signals.handlers` while registration.
    2. Added `is_staff` related logic in `managers.py`
    3. Renamed `type` to `prop` in `models.OTPValidation`
    4. Added `DESTINATION_CHOICES` in `models.OTPValidation`
    5. Optimized `views.py` which includes removing all dependencies from `drfaddons.views`, optimizing all the APIs.
    6. Created `utils.py`. It contains functions from `views.py`
    7. Added `jwt_payload_handler` from `drfaddons` to `auth` as it is directly related to `drf_user`
    8. Optimized `urls.py` to do multiple task from same url via different method
    9. Renamed `serializer` to `serializers`
    9. Optimized `serializers` to use same class for multiple purpose

---
+++ drf_user/signals/handlers.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django.contrib.auth import get_user_model
+
+from drf_user.models import OTPValidation
+
+
+@receiver(post_save, sender=get_user_model())
+def post_register(sender, instance, created, **kwargs):
+    from drf_user import user_settings
+
+    from drfaddons.add_ons import send_message
+
+    if created:
+        if user_settings['REGISTRATION']['SEND_MAIL']:
+            send_message(message=user_settings['REGISTRATION']['TEXT_MAIL_BODY'],
+                         subject=user_settings['REGISTRATION']['MAIL_SUBJECT'],
+                         recip=[instance.email], recip_email=[instance.email])
