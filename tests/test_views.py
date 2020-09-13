f329a0770e3a5fdf648eb32dfda79de7d38dd3d9
Sumit Singh <sumit.singh4613@gmail.com>
2020-09-13 18:25:27 +0530

    Test cases for OTPView API (#37)
    * add test cases for otp view
    
    * update docstrings

--- tests/test_views.py
+++ tests/test_views.py
@@ -197,3 +197,104 @@ class TestRegisterView(APITestCase):
         assert response.status_code == 400
         assert "The email must be pre-validated via OTP." in response.json()["email"]
         assert "The mobile must be pre-validated via OTP." in response.json()["mobile"]
+
+
+class TestOTPView(APITestCase):
+    """OTPView Test"""
+
+    def setUp(self) -> None:
+        """SetUp test data"""
+        self.user = baker.make("drf_user.User", email="user@example.com")
+        self.otp_user = baker.make(
+            "drf_user.OTPValidation", destination="user@example.com", otp=888383
+        )
+        self.otp_object = baker.make(
+            "drf_user.OTPValidation", destination="email@django.com", otp=123456
+        )
+        self.url = reverse("OTP")
+
+    def test_request_otp_on_email(self):
+        """
+        Checks when destination and email is passed to OTPView is sends otp on mail
+        """
+
+        response = self.client.post(
+            self.url, {"destination": "email@django.com", "email": "email@django.com"}
+        )
+
+        assert response.status_code == 201
+        assert "Message sent successfully!" in response.json()["message"]
+
+    def test_request_otp_on_email_and_mobile(self):
+        """
+        Checks when mobile as destination and email is passed to OTPView
+        it sends otp on mail as well as on mobile
+        """
+
+        response = self.client.post(
+            self.url, {"destination": 1231242492, "email": "email@django.com"}
+        )
+
+        assert response.status_code == 201
+        assert "Message sent successfully!" in response.json()["message"]
+
+    def test_raise_api_exception_when_email_invalid(self):
+        """Checks OTPView raises validation error when email/mobile is invalid"""
+
+        response = self.client.post(
+            self.url, {"destination": "a.b", "email": "abc@d.com"}
+        )
+
+        assert response.status_code == 500
+        assert "Server configuration error occured:" in response.json()["detail"]
+
+    def test_raise_validation_error_when_email_not_response_when_user_is_new(self):
+        """
+        Checks OTPView raises validation error when new user
+        only passes destination and not email
+        """
+
+        response = self.client.post(self.url, {"destination": "email@django.com"})
+
+        assert (
+            "email field is compulsory while verifying a non-existing user's OTP."
+            in response.json()["non_field_errors"]
+        )
+        assert response.status_code == 400
+
+    def test_raise_validation_error_when_is_login_response_when_user_is_new(self):
+        """
+        Checks OTPView raises validation error when new user
+        only passes is_login
+        """
+
+        response = self.client.post(
+            self.url, {"destination": "email@django.com", "is_login": True}
+        )
+
+        assert "No user exists with provided details" in response.json()["detail"]
+        assert response.status_code == 404
+
+    def test_verify_otp_in_response(self):
+        """Check otp validation"""
+        response = self.client.post(
+            self.url,
+            {
+                "destination": "email@django.com",
+                "email": "email@django.com",
+                "verify_otp": 123456,
+            },
+        )
+
+        assert response.status_code == 202
+        assert "OTP Validated successfully!" in response.json()["OTP"]
+
+    def test_is_login_in_response(self):
+        """Check user login with OTP"""
+
+        response = self.client.post(
+            self.url,
+            {"destination": "user@example.com", "verify_otp": 888383, "is_login": True},
+        )
+
+        assert response.status_code == 202
