791407beed9a08fd1d70a87796e753aca886fb3d
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2020-09-12 18:56:54 -0700

    Adds test cases for UserAccount View (#24)
    * adds test case for user account get object method
    
    * adds test case for get user account API
    
    * fixed typo
    
    * adds test case for update user account API
    
    * adds username equal assertion in get user account API

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,11 +2,15 @@
 import pytest
 from django.test import Client
 from django.test import TestCase
+from django.test.client import RequestFactory
 from django.urls import reverse
 from model_bakery import baker
 from rest_framework import status
+from rest_framework.test import force_authenticate
 
+from drf_user.models import AuthTransaction
 from drf_user.models import User
+from drf_user.views import RetrieveUpdateUserAccountView
 
 
 class LoginViewTest(TestCase):
@@ -50,3 +54,82 @@ class LoginViewTest(TestCase):
             reverse("Login"), data={"username": "user", "password": "pass1234"}
         )
         self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+class UserAccountViewTest(TestCase):
+    """User Account View Test"""
+
+    def setUp(self) -> None:
+        """Create RequestFactory object to call the API"""
+        self.factory = RequestFactory()
+
+        """Create User object using model_bakery"""
+        self.user = baker.make(
+            "drf_user.User",
+            username="user",
+            email="user@email.com",
+            mobile=1234569877,
+        )
+
+        """Create auth_transaction object using model_bakery"""
+        self.auth_transaction = baker.make(
+            "drf_user.AuthTransaction",
+            created_by=self.user,
+        )
+
+    @pytest.mark.django_db
+    def test_object_created(self):
+        """Check if AuthTransaction object created or not"""
+        assert AuthTransaction.objects.count() == 1
+
+    @pytest.mark.django_db
+    def test_get_object_method(self):
+        """Create request object using factory"""
+        request = self.factory.get(reverse("Retrieve Update Profile"))
+
+        """Simulating that self.user has made the request"""
+        request.user = self.user
+
+        """Creates and sets up the RetrieveUpdateUserAccountView"""
+        view = RetrieveUpdateUserAccountView()
+        view.setup(request)
+
+        self.assertEqual(view.get_object(), self.user)
+
+    @pytest.mark.django_db
+    def test_get_user_account_view(self):
+        """Create request object using factory"""
+        request = self.factory.get(reverse("Retrieve Update Profile"))
+
+        """Authenticating the request"""
+        force_authenticate(
+            request=request, user=self.user, token=self.auth_transaction.token
+        )
+
+        """Creates and sets up the RetrieveUpdateUserAccountView"""
+        view = RetrieveUpdateUserAccountView.as_view()
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["username"], self.user.username)
+
+    @pytest.mark.django_db
+    def test_update_user_account_view(self):
+        """Create request object using factory"""
+        request = self.factory.patch(
+            reverse("Retrieve Update Profile"),
+            data={"username": "updated_username"},
+            content_type="application/json",
+        )
+
+        """Authenticating the request"""
+        force_authenticate(
+            request=request, user=self.user, token=self.auth_transaction.token
+        )
+
+        """Creates and sets up the RetrieveUpdateUserAccountView"""
+        view = RetrieveUpdateUserAccountView.as_view()
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.user.username, "updated_username")
