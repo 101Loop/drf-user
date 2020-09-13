e1a26028c410c06d5630876c0f32065080fbd46e
Restyled.io <commits@restyled.io>
2020-09-13 12:54:06 +0000

    Restyled by black

--- tests/settings.py
+++ tests/settings.py
@@ -53,12 +53,7 @@ ROOT_URLCONF = "drf_user.urls"
 
 # DATABASE
 # ------------------------------------------------------------------------------
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
-    }
-}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:",}}
 
 AUTH_USER_MODEL = "drf_user.User"
 
