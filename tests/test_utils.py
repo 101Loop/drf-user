a766020e2ae24a7c837e893aba81fcab4d4f62ce
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2020-09-13 17:18:41 +0530

    adds test cases for generate_otp function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
 """Tests for drf_user/utils.py module"""
+import datetime
+
 import pytest
 from django.test import TestCase
+from django.utils import timezone
 from model_bakery import baker
 
 from drf_user import utils as utils
@@ -57,3 +60,38 @@ class TestCheckValidation(TestCase):
     def test_check_non_validated_object(self):
         """Check if the value is not validated"""
         assert not utils.check_validation("user1@email.com")
+
+
+class TestGenerateOTP(TestCase):
+    """generate_otp Test"""
+
+    @pytest.mark.django_db
+    def test_generate_otp(self):
+        """Check generate_otp successfully generates OTPValidation object or not"""
+        utils.generate_otp("email", "user1@email.com")
+        assert OTPValidation.objects.count() == 1
+
+    @pytest.mark.django_db
+    def test_generate_otp_reactive_past(self):
+        """
+        Check generate_otp generates a new otp if the reactive time is yet to be over
+        """
+        otp_validation1 = utils.generate_otp("email", "user1@email.com")
+        otp_validation2 = utils.generate_otp("email", "user1@email.com")
+        assert otp_validation1.otp != otp_validation2.otp
+
+    @pytest.mark.django_db
+    def test_generate_otp_reactive_future(self):
+        """
+        Check generate_otp returns the same otp if the reactive time is already over
+        """
+        otp_validation1 = utils.generate_otp("email", "user1@email.com")
+
+        """
+        Simulating that the reactive time is already been over 5 minutes ago
+        """
+        otp_validation1.reactive_at = timezone.now() + datetime.timedelta(minutes=5)
+        otp_validation1.save()
+
+        otp_validation2 = utils.generate_otp("email", "user1@email.com")
+        assert otp_validation1.otp == otp_validation2.otp
