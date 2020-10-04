4795f1ec61e52a0737a89b0608c5c53f4ef4b1e3
Restyled.io <commits@restyled.io>
2020-10-04 11:34:05 +0000

    Restyled by black

--- Demo App/demo_app/demo_app/settings.py
+++ Demo App/demo_app/demo_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tlr7z*^%8^l@0)%%e^xyvolo$8l2j1$)!t(gs+7m!7ine8eei-'
+SECRET_KEY = "tlr7z*^%8^l@0)%%e^xyvolo$8l2j1$)!t(gs+7m!7ine8eei-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,56 +32,56 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'drf_user',
-    'drfaddons',
-    'rest_framework',
-    'django_filters',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "drf_user",
+    "drfaddons",
+    "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'demo_app.urls'
+ROOT_URLCONF = "demo_app.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'demo_app.wsgi.application'
+WSGI_APPLICATION = "demo_app.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -91,26 +91,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -122,13 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 AUTHENTICATION_BACKENDS = [
-    'drf_user.auth.MultiFieldModelBackend',
+    "drf_user.auth.MultiFieldModelBackend",
 ]
 
-AUTH_USER_MODEL = 'drf_user.User'
+AUTH_USER_MODEL = "drf_user.User"
 
 JWT_AUTH = {
     "JWT_ENCODE_HANDLER": "rest_framework_jwt.utils.jwt_encode_handler",
@@ -154,28 +148,26 @@ JWT_AUTH = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'drfaddons.auth.JSONWebTokenAuthenticationQS',
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": ("drfaddons.auth.JSONWebTokenAuthenticationQS",),
 }
 
 USER_SETTINGS = {
-    'DEFAULT_ACTIVE_STATE': True,
-    'OTP': {
-        'LENGTH': 7,
-        'ALLOWED_CHARS': '1234567890',
-        'VALIDATION_ATTEMPTS': 3,
-        'SUBJECT': 'OTP for Verification',
-        'COOLING_PERIOD': 3
+    "DEFAULT_ACTIVE_STATE": True,
+    "OTP": {
+        "LENGTH": 7,
+        "ALLOWED_CHARS": "1234567890",
+        "VALIDATION_ATTEMPTS": 3,
+        "SUBJECT": "OTP for Verification",
+        "COOLING_PERIOD": 3,
     },
-    'MOBILE_VALIDATION': True,
-    'EMAIL_VALIDATION': True,
-    'REGISTRATION': {
-        'SEND_MAIL': False,
-        'SEND_MESSAGE': False,
-        'MAIL_SUBJECT': 'Welcome to DRF-USER',
-        'SMS_BODY': 'Your account has been created',
-        'TEXT_MAIL_BODY': 'Your account has been created.',
-        'HTML_MAIL_BODY': 'Your account has been created.'
-    }
+    "MOBILE_VALIDATION": True,
+    "EMAIL_VALIDATION": True,
+    "REGISTRATION": {
+        "SEND_MAIL": False,
+        "SEND_MESSAGE": False,
+        "MAIL_SUBJECT": "Welcome to DRF-USER",
+        "SMS_BODY": "Your account has been created",
+        "TEXT_MAIL_BODY": "Your account has been created.",
+        "HTML_MAIL_BODY": "Your account has been created.",
+    },
 }
