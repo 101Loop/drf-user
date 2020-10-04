b49a7719fc25fbfad85d2f20014055a9b5d717d3
Restyled.io <commits@restyled.io>
2020-10-04 11:34:06 +0000

    Restyled by reorder-python-imports

--- Demo App/demo_app/demo_app/asgi.py
+++ Demo App/demo_app/demo_app/asgi.py
@@ -6,7 +6,6 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
 import os
 
 from django.core.asgi import get_asgi_application
--- Demo App/demo_app/demo_app/settings.py
+++ Demo App/demo_app/demo_app/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
-from pathlib import Path
 import datetime
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
