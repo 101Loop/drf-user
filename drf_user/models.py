2f4db934ddd3c07aab5ae8ddb4d14b36b0c1c734
Sumit Singh <sumit.singh4613@gmail.com>
2020-10-18 10:53:49 +0530

    remove unneccessary import

--- drf_user/models.py
+++ drf_user/models.py
@@ -1,6 +1,4 @@
 """Models for drf-user"""
-from __future__ import absolute_import
-
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import PermissionsMixin
