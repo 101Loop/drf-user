9ee6999cec3511fb02df7a4f52bf305b9a6d5acd
Sumit Singh <sumit.singh4613@gmail.com>
2020-12-12 16:49:03 +0530

    integrates swagger in example app, so that apis can be exported

--- example/demo_app/settings.py
+++ example/demo_app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "drfaddons",
     "rest_framework",
     "django_filters",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- example/demo_app/urls.py
+++ example/demo_app/urls.py
@@ -16,8 +16,39 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.urls import re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="drf-users api",
+        default_version="v1",
+        description="This Django User Model is customised user model keeping in mind the practical need (esp. in India).",  # noqa: E501
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="devs@101loop.com"),
+        license=openapi.License(name="GPL-3.0 License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("drf_user.urls")),
+    # swagger urls
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
