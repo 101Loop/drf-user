cddee126d8382d20b3869c145eabfca7f13d6406
Sumit Singh <sumit.singh4613@gmail.com>
2021-07-17 18:18:43 +0530

    fix classifiers

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries :: Django App",
+        "Topic :: Software Development :: Libraries",
         "Topic :: Internet :: WWW/HTTP",
     ),
 )
